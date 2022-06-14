8c79d03bc8d1a04de0428aad0df5228733d2ced2
JMaria <jmariadelacruzsanchez@gmail.com>
2022-06-14 14:18:54 +0200

    update

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,11 +1,16 @@
+/*
+    To execute script on local network
+    1. `yarn hardhat node``
+    2. `yarn hardhat run scripts/mint-and-list.js --network localhost`
+*/
 const { ethers, network } = require("hardhat")
-const { moveBlocks } = require("../utils/move-blocks")
+//const { moveBlocks } = require("../utils/move-blocks")
 
 const PRICE = ethers.utils.parseEther("0.1")
 
 async function mintAndList() {
     const nftMarketplace = await ethers.getContract("NftMarketplace")
-    let basicNft
+    const basicNft = await ethers.getContract("BasicNft")
     
     console.log("Minting NFT...")
     const mintTx = await basicNft.mintNft()
@@ -18,10 +23,10 @@ async function mintAndList() {
     const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE)
     await tx.wait(1)
     console.log("NFT Listed!")
-    if (network.config.chainId == 31337) {
+    /*if (network.config.chainId == 31337) {
         // Moralis has a hard time if you move more than 1 at once!
         await moveBlocks(1, (sleepAmount = 1000))
-    }
+    }*/
 }
 
 mintAndList()
